a3442015bc203d925d931a796e7e636bdc093d89
phainon2367 <robertyu2024@163.com>
2026-04-11 19:43:13 +0800

    [feat]: updated unittest for get_attraction, get_attraction passed the test.

--- testSimpleAgent.py
+++ testSimpleAgent.py
@@ -6,6 +6,11 @@ class TestSimpleAgent(unittest.TestCase):
     def testGetWeather(self):
         weather = get_weather("Shanghai")
         print(weather)
+
+    def testGetAttraction(self):
+        weather = get_weather("Shanghai")
+        attraction = get_attraction("Shanghai", weather)
+        print(attraction)
         
 if __name__ == '__main__':
     unittest.main()
